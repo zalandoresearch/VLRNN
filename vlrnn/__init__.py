5ceee5b583c8150c5a4c8d195a539c5ca852e52c
Roland Vollgraf <roland.vollgraf@gmail.com>
2021-06-29 15:18:19 +0200

    new: modular VLRNN to replace BlockRNN

--- vlrnn/__init__.py
+++ vlrnn/__init__.py
@@ -1,2 +1,2 @@
-from .block_rnn import SequenceStruct, BlockRNN, OutputModule, RNNModule, BaseRNN
+from .block_rnn import SequenceStruct, BlockRNN, OutputModule, RNNModule, BaseRNN, PlainRNN, VLRNN
 from .utilities import *
